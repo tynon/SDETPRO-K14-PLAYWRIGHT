6db33f44b9d5c9af012da1507a2a0884fefb6997
khoa <freedom3000@gmail.com>
2024-04-04 00:59:55 +0700

    login test

--- tests/Login.ts
+++ tests/Login.ts
@@ -1,1 +1,14 @@
+//import {chromium, Browser, Page, BrowserContext} from 'playwright';
 import {test} from '@playwright/test';
+
+test('Login test', async ({page}) => {
+    //const browser: Browser = await chromium.launch();
+    //const context: BrowserContext = await browser.newContext();
+    //const page: Page = await context.newPage();
+
+    await page.goto("https://playwright.dev");
+
+    await page.waitForTimeout(3000);
+
+
+})
